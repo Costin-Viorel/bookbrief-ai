f12c9a456792b3c316ae5d39f521f1113c5d26fd
Viorel <craciuneanucostin20@stud.ase.ro>
2026-05-10 13:49:28 +0300

    Npm run build fix - next-auth error

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,9 +1,9 @@
-import NextAuth from "next-auth";
+import NextAuth, { type AuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { getCollection } from "@/lib/mongodb";
 import bcrypt from "bcryptjs";
 
-export const authOptions = {
+export const authOptions: AuthOptions = {
   providers: [
     CredentialsProvider({
       name: "Credentials",
